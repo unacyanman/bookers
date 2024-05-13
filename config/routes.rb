d5dfc73ab96d8f99a9a1e9165af63fe3750c1829
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-13 12:38:03 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   get 'lists/new'
-  post 'lists' => 'lists#create'
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
