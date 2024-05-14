bc51cfbdd0c4df1177f77ba47dffeff8c45c91f8
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-14 13:29:36 +0000

    [Update]index link_to

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'new' => 'books#new'
   post 'book' => 'books#create'
   get 'books' => 'books#index'
-  get 'show' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'edit' => 'books#edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
