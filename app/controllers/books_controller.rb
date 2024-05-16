241537b33ae11b55f2231e1001b1d5d9575dda38
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-16 10:30:54 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
-  
-  def new
-    @book = Book.new
-  end
+
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.all
+      render :index
+    end
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
   end
 
   def show
