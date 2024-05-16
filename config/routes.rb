d1f2720f703c5fc283389209ba0a2fd9bef8484b
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-16 04:57:26 +0000

    [Update]update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
-  get 'new' => 'books#new'
-  post 'book' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'edit' => 'books#edit'
+  # get 'new' => 'books#new'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show'
+  # patch 'books/:id' => 'books#update'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  
+  resources :books
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
